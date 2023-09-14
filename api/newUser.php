bc7bec475e4de3a272b7fb5144ce1c4be580827f
Joaquin minatel <joacominatel@gmail.com>
2023-09-14 17:56:17 -0300

    Update user if exists

--- api/newUser.php
+++ api/newUser.php
@@ -5,7 +5,7 @@ include_once '../php/db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Leer el JSON desde la solicitud
     $data = json_decode(file_get_contents("php://input"));
-
+    
     // Obtener los valores del JSON
     $workday_id = $data->workday_id;
     $nombre = $data->nombre;
@@ -14,11 +14,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $modelo = $data->modelo;
     $serie = $data->serie;
 
-    // Insertar datos en la base de datos
-    $sql = "INSERT INTO usuarios (workday_id, nombre, apellido, marca, modelo, serie) 
-            VALUES (?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("isssss", $workday_id, $nombre, $apellido, $marca, $modelo, $serie);
+    // Consulta para verificar si workday_id existe
+    $sql = "SELECT workday_id FROM usuarios WHERE workday_id = '$workday_id'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Actualizar datos en la base de datos
+        $sql = "UPDATE usuarios SET nombre = ?, apellido = ?, marca = ?, modelo = ?, serie = ? WHERE workday_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssss", $nombre, $apellido, $marca, $modelo, $serie, $workday_id);
+    } else {
+        // Insertar datos en la base de datos
+        $sql = "INSERT INTO usuarios (workday_id, nombre, apellido, marca, modelo, serie) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssss", $workday_id, $nombre, $apellido, $marca, $modelo, $serie);
+    }
 
     if ($stmt->execute()) {
         echo "Datos insertados con éxito";
